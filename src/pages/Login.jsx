38e5c209edba382c3c1035ccfda89850a5a3b4b2
Hongfei01 <hongfeidu3@gmail.com>
2024-08-02 10:45:04 +0800

    edit Register and Login components

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,39 @@
 import { Link } from 'react-router-dom';
+
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { FormRow, Logo } from '../components';
 const Login = () => {
   return (
-    <div>
-      <h1>Login Page</h1>
-      <Link to='/register'>Register Page</Link>
-    </div>
+    <Wrapper>
+      <form className='form' method='POST'>
+        <Logo />
+        <h4>Login</h4>
+        <FormRow
+          type='email'
+          name='email'
+          defaultValue={'test@gmail.com'}
+          labelText={'Email'}
+        />
+        <FormRow
+          type={'password'}
+          labelText={'Password'}
+          name={'password'}
+          defaultValue={'qawsedrf'}
+        />
+        <button type='submit' className='btn btn-block'>
+          Submit
+        </button>
+        <button type='button' className='btn btn-block'>
+          Explore the app
+        </button>
+        <p>
+          Not a member yet?
+          <Link to='/register' className='member-btn'>
+            Register
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,26 +1,18 @@
 import { Link } from 'react-router-dom';
 
-import Logo from '../components/Logo';
 import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { FormRow, Logo } from '../components';
 const Register = () => {
   return (
     <Wrapper>
       <form className='form'>
         <Logo />
         <h4>Register</h4>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
-            name
-          </label>
-          <input
-            type='text'
-            id='name'
-            name='name'
-            required
-            className='form-input'
-            defaultValue={'john'}
-          />
-        </div>
+        <FormRow type='text' labelText={'First Name'} name={'name'} />
+        <FormRow type='text' labelText={'Last Name'} name={'lastName'} />
+        <FormRow type='text' labelText={'Location'} name={'location'} />
+        <FormRow type='email' labelText={'Email'} name={'email'} />
+        <FormRow type='password' labelText={'Password'} name={'password'} />
         <button type='submit' className='btn btn-block'>
           submit
         </button>
