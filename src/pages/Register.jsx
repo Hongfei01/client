24992b0d600a6db9f63e06e93f29705a02123d48
Hongfei01 <hongfeidu3@gmail.com>
2024-08-02 09:33:02 +0800

    edit Register component

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,10 +1,37 @@
 import { Link } from 'react-router-dom';
+
+import Logo from '../components/Logo';
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
 const Register = () => {
   return (
-    <div>
-      <h1>Register Page</h1>
-      <Link to='/login'>Login Page</Link>
-    </div>
+    <Wrapper>
+      <form className='form'>
+        <Logo />
+        <h4>Register</h4>
+        <div className='form-row'>
+          <label htmlFor='name' className='form-label'>
+            name
+          </label>
+          <input
+            type='text'
+            id='name'
+            name='name'
+            required
+            className='form-input'
+            defaultValue={'john'}
+          />
+        </div>
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+        <p>
+          Already a member?
+          <Link to='/login' className='member-btn'>
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 export default Register;
